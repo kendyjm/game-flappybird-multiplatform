0c55d0c1d6a1827e5411e78b8beb08f98d561041
Kendy JEAN-MARIE <kendyjm@gmail.com>
2016-11-27 18:12:04 +0100

    Brent Aureli, Part 1: LibGDX - How To Make Video Games: Flappy Bird

---
+++ desktop/src/com/kendy/game/flappybird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kendy.game.flappybird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kendy.game.flappybird.FlappyBrid;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyBrid(), config);
+	}
+}
